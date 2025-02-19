a664bc4277bbcb9e6b3e8a0542cb3dc724110936
Xu Hao <xuhao24fall@gmail.com>
2025-02-19 09:19:13 +0000

    Complete scapying miit data

--- miit_spider/settings.py
+++ miit_spider/settings.py
@@ -20,7 +20,7 @@ NEWSPIDER_MODULE = "miit_spider.spiders"
 ROBOTSTXT_OBEY = True
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
-CONCURRENT_REQUESTS = 16
+CONCURRENT_REQUESTS = 128
 
 # Configure a delay for requests for the same website (default: 0)
 # See https://docs.scrapy.org/en/latest/topics/settings.html#download-delay
